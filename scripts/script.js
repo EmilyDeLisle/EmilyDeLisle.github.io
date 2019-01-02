56c3322ba272a6f305c405cf18f7f87a09dddce4
Emily DeLisle <emily.delisle@gmail.com>
2019-01-01 19:15:42 -0800

    Refined JavaScript and some styling.

--- scripts/script.js
+++ scripts/script.js
@@ -1,42 +1,15 @@
-// Navigation controls
 $(document).ready(function() {
-  $("#aboutLink").on('click', function (e) {
+
+  // Navigation animations
+  $(".pageNav").click(function(e) {
     e.preventDefault();
     $("html, body").animate({
       scrollTop: $($(this).attr('href')).offset().top
     }, 500, 'linear');
   });
 
-  $("#projectsLink").on('click', function (e) {
-    e.preventDefault();
-    $("html, body").animate({
-      scrollTop: $($(this).attr('href')).offset().top
-    }, 500, 'linear');
-  });
-
-  $("#contactLink").on('click', function (e) {
-    e.preventDefault();
-    $("html, body").animate({
-      scrollTop: $($(this).attr('href')).offset().top
-    }, 500, 'linear');
-  });
-
-  $("#navLink").on('click', function (e) {
-    e.preventDefault();
-    $("html, body").animate({
-      scrollTop: $($(this).attr('href')).offset().top
-    }, 500, 'linear');
-  });
-
-  $("#topLink").on('click', function (e) {
-    e.preventDefault();
-    $("html, body").animate({
-      scrollTop: $($(this).attr('href')).offset().top
-    }, 500, 'linear');
-  });
-
-  // Technology animations
-  $("#htmlImage").hover(
+  // Technology and project image animations
+  $(".hover").hover(
     function() {
       $(this).children("div").fadeIn();
       $(this).children("img").animate({opacity: "0.3"});},
@@ -45,112 +18,4 @@ $(document).ready(function() {
       $(this).children("img").animate({opacity: "1"});}
   );
 
-  $("#cssImage").hover(
-    function() {
-      $(this).children("div").fadeIn();
-      $(this).children("img").animate({opacity: "0.3"});},
-    function() {
-      $(this).children("div").fadeOut();
-      $(this).children("img").animate({opacity: "1"});}
-  );
-
-  $("#jsImage").hover(
-    function() {
-      $(this).children("div").fadeIn();
-      $(this).children("img").animate({opacity: "0.3"});},
-    function() {
-      $(this).children("div").fadeOut();
-      $(this).children("img").animate({opacity: "1"});}
-  );
-
-  $("#bsImage").hover(
-    function() {
-      $(this).children("div").fadeIn();
-      $(this).children("img").animate({opacity: "0.3"});},
-    function() {
-      $(this).children("div").fadeOut();
-      $(this).children("img").animate({opacity: "1"});}
-  );
-
-  $("#jqImage").hover(
-    function() {
-      $(this).children("div").fadeIn();
-      $(this).children("img").animate({opacity: "0.3"});},
-    function() {
-      $(this).children("div").fadeOut();
-      $(this).children("img").animate({opacity: "1"});}
-  );
-
-  $("#nodeImage").hover(
-    function() {
-      $(this).children("div").fadeIn();
-      $(this).children("img").animate({opacity: "0.3"});},
-    function() {
-      $(this).children("div").fadeOut();
-      $(this).children("img").animate({opacity: "1"});}
-  );
-
-  $("#javaImage").hover(
-    function() {
-      $(this).children("div").fadeIn();
-      $(this).children("img").animate({opacity: "0.3"});},
-    function() {
-      $(this).children("div").fadeOut();
-      $(this).children("img").animate({opacity: "1"});}
-  );
-
-  $("#psImage").hover(
-    function() {
-      $(this).children("div").fadeIn();
-      $(this).children("img").animate({opacity: "0.3"});},
-    function() {
-      $(this).children("div").fadeOut();
-      $(this).children("img").animate({opacity: "1"});}
-  );
-
-  // Project Animations
-  $("#plantImage").hover(
-    function() {
-      $(this).children("div").fadeIn();
-      $(this).children("img").animate({opacity: "0.3"});},
-    function() {
-      $(this).children("div").fadeOut();
-      $(this).children("img").animate({opacity: "1"});}
-  );
-
-  $("#medImage").hover(
-    function() {
-      $(this).children("div").fadeIn();
-      $(this).children("img").animate({opacity: "0.3"});},
-    function() {
-      $(this).children("div").fadeOut();
-      $(this).children("img").animate({opacity: "1"});}
-  );
-
-  $("#weatherImage").hover(
-    function() {
-      $(this).children("div").fadeIn();
-      $(this).children("img").animate({opacity: "0.3"});},
-    function() {
-      $(this).children("div").fadeOut();
-      $(this).children("img").animate({opacity: "1"});}
-  );
-
-  $("#wikiImage").hover(
-    function() {
-      $(this).children("div").fadeIn();
-      $(this).children("img").animate({opacity: "0.3"});},
-    function() {
-      $(this).children("div").fadeOut();
-      $(this).children("img").animate({opacity: "1"});}
-  );
-
-  $("#randomImage").hover(
-    function() {
-      $(this).children("div").fadeIn();
-      $(this).children("img").animate({opacity: "0.3"});},
-    function() {
-      $(this).children("div").fadeOut();
-      $(this).children("img").animate({opacity: "1"});}
-  );
 });
